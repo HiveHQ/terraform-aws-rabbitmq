fee6306804f74d29c11cc2d1070ef1484e4d33e8
Patrick Hannigan <patrick@hive.co>
2019-09-19 16:26:24 -0400

    removing HA policy

--- main.tf
+++ main.tf
@@ -48,13 +48,11 @@ data "template_file" "cloud-init" {
   template = file("${path.module}/cloud-init.yaml")
 
   vars = {
-    sync_node_count = 3
     asg_name        = local.cluster_name
     region          = data.aws_region.current.name
     admin_password  = random_string.admin_password.result
     rabbit_password = random_string.rabbit_password.result
     secret_cookie   = random_string.secret_cookie.result
-    message_timeout = 3 * 24 * 60 * 60 * 1000 # 3 days
   }
 }
 
